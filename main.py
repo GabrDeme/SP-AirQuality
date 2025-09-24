299cc87eec0039739dbb76712b64ffc532d688aa
Gabriel Demétrio <125273594+GabrDeme@users.noreply.github.com>
2025-09-24 19:53:42 -0300

    API organizada

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timezone
 from flask import Flask, Response, request
 from flask_sqlalchemy import SQLAlchemy
 import json
+import paho.mqtt.client as mqtt
 
 # -----------------------------
 # Configuração inicial
@@ -13,9 +15,78 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 mydb = SQLAlchemy(app)
 
-# -----------------------------
-# Modelos do banco
-# -----------------------------
+# =================================================================================== #
+# Conexão MQTT com os sensores
+# =================================================================================== #
+
+mqtt_data = {}
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("projeto_integrado/SENAI134/Cienciadedados/grupo1")
+
+def on_message(client, userdata, msg):
+    global mqtt_data
+    # Decodifica a mensagem recebida de bytes para string
+    payload = msg.payload.decode('utf-8')
+   
+    # Converte a string JSON em um dicionário Python
+    mqtt_data = json.loads(payload)
+   
+    # Imprime a mensagem recebida
+    print(f"Received message: {mqtt_data}")
+
+    # Adiciona o contexto da aplicação para a manipulação do banco de dados
+    with app.app_context():
+        try:
+            temperatura = mqtt_data.get('temperature')
+            pressao = mqtt_data.get('pressure')
+            altitude = mqtt_data.get('altitude')
+            umidade = mqtt_data.get('humidity')
+            co2 = mqtt_data.get('co2')
+            timestamp_unix = mqtt_data.get('timestamp')
+
+            if timestamp_unix is None:
+                print("Timestamp não encontrado no payload")
+                return
+
+            # Converte timestamp Unix para datetime
+            try:
+                timestamp = datetime.fromtimestamp(int(timestamp_unix), tz=timezone.utc)
+            except (ValueError, TypeError) as e:
+                print(f"Erro ao converter timestamp: {str(e)}")
+                return
+
+            # Cria o objeto Registro com os dados
+            new_data = Leituras(
+                temperatura=temperatura,
+                pressao=pressao,
+                altitude=altitude,
+                umidade=umidade,
+                co2=co2,
+                tempo_registro=timestamp
+            )
+
+            # Adiciona o novo registro ao banco de dados
+            mydb.session.add(new_data)
+            mydb.session.commit()
+            print("Dados inseridos no banco de dados com sucesso")
+
+        except Exception as e:
+            print(f"Erro ao processar os dados do MQTT: {str(e)}")
+            mydb.session.rollback()
+
+mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+mqtt_client.connect("test.mosquitto.org", 1883, 60)
+
+def start_mqtt():
+    mqtt_client.loop_start()
+
+# =================================================================================== #
+# Modelo de dados - Leituras dos sesnsores
+# =================================================================================== #
 class Leituras(mydb.Model):
     __tablename__ = "tb_leituras"
     idLeitura = mydb.Column(mydb.Integer, primary_key=True)
@@ -41,6 +112,9 @@ class Leituras(mydb.Model):
             "altitude": self.altitude,
         }
 
+# =================================================================================== #
+# Modelo de dados - API Google Air Quality
+# =================================================================================== #
 class Poluentes(mydb.Model):
     __tablename__ = "tb_poluentes"
     idPoluente = mydb.Column(mydb.Integer, primary_key=True)
@@ -96,20 +170,18 @@ class Recomendacao(mydb.Model):
             "recomendacao": self.recomendacao
         }
     
+def gera_resposta(status, conteudo, mensagem=False):
+    body = {"conteudo": conteudo}
+    if mensagem:
+        body["mensagem"] = mensagem
+    return Response(json.dumps(body), status=status, mimetype="application/json")
 
-
-# Configuração de conexão com o banco
-# %40 -> faz o papel de @
-# 1 - Usuario (root) 2 - Senha(Senai540134) 3 - localhost (127.0.0.1) 4 - nome do banco
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Senai%40134@127.0.0.1/db_aqi"
-
-
-# -----------------------------
-# Rotas CRUD - Leituras
-# -----------------------------
+# =================================================================================== #
+# Requisições API - Leituras
+# =================================================================================== #
 
 # GET - Todas as leituras
-@app.route("/aqi/tb_leituras", methods=["GET"])
+@app.route("/leitura", methods=["GET"])
 def get_leituras():
     leituras = Leituras.query.all()
     leituras_json = [l.to_json() for l in leituras]
@@ -117,7 +189,7 @@ def get_leituras():
 
 
 # GET - Leitura por ID
-@app.route("/aqi/tb_leituras/<int:idLeitura>", methods=["GET"])
+@app.route("/leitura/<int:idLeitura>", methods=["GET"])
 def get_leitura_id(idLeitura):
     leitura = Leituras.query.filter_by(idLeitura=idLeitura).first()
     if not leitura:
@@ -125,8 +197,8 @@ def get_leitura_id(idLeitura):
     return gera_resposta(200, leitura.to_json())
 
 
-# POST - Criar leitura
-@app.route("/aqi/tb_leituras", methods=["POST"])
+# POST - Cria leitura
+@app.route("/leitura", methods=["POST"])
 def criar_leitura():
     requisicao = request.get_json()
     try:
@@ -148,8 +220,8 @@ def criar_leitura():
         return gera_resposta(400, {}, "Erro ao cadastrar leitura")
 
 
-# PUT - Atualizar leitura
-@app.route("/aqi/tb_leituras/<int:idLeitura>", methods=["PUT"])
+# PUT - Atualiza leitura
+@app.route("/leitura/<int:idLeitura>", methods=["PUT"])
 def atualizar_leitura(idLeitura):
     leitura = Leituras.query.filter_by(idLeitura=idLeitura).first()
     if not leitura:
@@ -176,9 +248,12 @@ def atualizar_leitura(idLeitura):
         print("Erro:", e)
         return gera_resposta(400, {}, "Erro ao atualizar leitura")
 
+# =================================================================================== #
+# Requisições API - Poluentes
+# =================================================================================== #
 
 # GET - Todos os poluentes
-@app.route("/aqi/tb_poluentes", methods=["GET"])
+@app.route("/poluente", methods=["GET"])
 def get_poluentes():
     poluentes = Poluentes.query.all()
     poluentes_json = [p.to_json() for p in poluentes]
@@ -186,7 +261,7 @@ def get_poluentes():
 
 
 # GET - Poluente por ID
-@app.route("/aqi/tb_poluentes/<int:idPoluente>", methods=["GET"])
+@app.route("/poluente/<int:idPoluente>", methods=["GET"])
 def get_poluente_id(idPoluente):
     poluente = Poluentes.query.filter_by(idPoluente=idPoluente).first()
     if not poluente:
@@ -194,8 +269,8 @@ def get_poluente_id(idPoluente):
     return gera_resposta(200, poluente.to_json())
 
 
-# POST - Criar poluente
-@app.route("/aqi/tb_poluentes", methods=["POST"])
+# POST - Cria poluente
+@app.route("/poluente", methods=["POST"])
 def criar_poluente():
     requisicao = request.get_json()
     try:
@@ -213,8 +288,8 @@ def criar_poluente():
         return gera_resposta(400, {}, "Erro ao cadastrar poluente")
 
 
-# PUT - Atualizar poluente
-@app.route("/aqi/tb_poluentes/<int:idPoluente>", methods=["PUT"])
+# PUT - Atualiza poluente
+@app.route("/poluente/<int:idPoluente>", methods=["PUT"])
 def atualizar_poluente(idPoluente):
     poluente = Poluentes.query.filter_by(idPoluente=idPoluente).first()
     if not poluente:
@@ -232,7 +307,38 @@ def atualizar_poluente(idPoluente):
         print("Erro:", e)
         return gera_resposta(400, {}, "Erro ao atualizar poluente")
 
+# =================================================================================== #
+# Requisições API - Qualidade
+# =================================================================================== #
 
+# GET - Todas as Qualidades
+@app.route('/qualidade', methods=['GET'])
+def get_qualidades():
+    try:
+        qualidades = Qualidade.query.all()
+        if not qualidades:
+            return gera_resposta(404, [], "Nenhuma qualidade encontrada.")
+        
+        qualidades_json = [q.to_json() for q in qualidades]
+        return gera_resposta(200, qualidades_json)
+    except Exception as e:
+        print('Erro:', e)
+        return gera_resposta(500, {}, "Ocorreu um erro interno no servidor.")
+    
+# GET - Qualidade por ID
+@app.route('/qualidade/<int:idQualidade>', methods=['GET'])
+def get_qualidade_por_id(idQualidade):
+    try:
+        qualidade = Qualidade.query.get(idQualidade)
+        if not qualidade:
+            return gera_resposta(404, {}, f"Qualidade com ID {idQualidade} não encontrada.")
+        return gera_resposta(200, qualidade.to_json())
+    except Exception as e:
+        print('Erro:', e)
+        return gera_resposta(500, {}, "Ocorreu um erro interno no servidor.")
+
+# POST
+@app.route("/qualidade", methods=["POST"])
 def cria_qualidade():
     body = request.get_json()
     try:
@@ -251,31 +357,38 @@ def cria_qualidade():
     except Exception as e:
         print('Erro:', e)
         return gera_resposta(400, {}, "Erro ao criar registro de qualidade.")
+    
+# =================================================================================== #
+# Requisições API - Recomendações
+# =================================================================================== #
 
-@app.route('/qualidade', methods=['GET'])
-def get_qualidades():
+# GET - Todas as Recomendações
+@app.route('/recomendacao', methods=['GET'])
+def get_recomendacoes():
     try:
-        qualidades = Qualidade.query.all()
-        if not qualidades:
-            return gera_resposta(404, [], "Nenhuma qualidade encontrada.")
+        recomendacoes = Recomendacao.query.all()
+        if not recomendacoes:
+            return gera_resposta(404, [], "Nenhuma recomendação encontrada.")
         
-        qualidades_json = [q.to_json() for q in qualidades]
-        return gera_resposta(200, qualidades_json)
+        recomendacoes_json = [r.to_json() for r in recomendacoes]
+        return gera_resposta(200, recomendacoes_json)
     except Exception as e:
         print('Erro:', e)
         return gera_resposta(500, {}, "Ocorreu um erro interno no servidor.")
     
-@app.route('/qualidade/<int:id>', methods=['GET'])
-def get_qualidade_por_id(id):
+# GET - Recomendação por ID
+@app.route('/recomendacao/<int:idRecomendacao>', methods=['GET'])
+def get_recomendacao_por_id(idRecomendacao):
     try:
-        qualidade = Qualidade.query.get(id)
-        if not qualidade:
-            return gera_resposta(404, {}, f"Qualidade com ID {id} não encontrada.")
-        return gera_resposta(200, qualidade.to_json())
+        recomendacao = Recomendacao.query.get(idRecomendacao)
+        if not recomendacao:
+            return gera_resposta(404,{}, f"Recomendação com ID {idRecomendacao} não encontrada.")
+        return gera_resposta(200, recomendacao.to_json())
     except Exception as e:
         print('Erro:', e)
         return gera_resposta(500, {}, "Ocorreu um erro interno no servidor.")
-    
+
+# POST - Cria Recomendacao
 @app.route('/recomendacao', methods=['POST'])
 def cria_recomendacao():
     body = request.get_json()
@@ -291,40 +404,8 @@ def cria_recomendacao():
     except Exception as e:
         print('Erro:', e)
         return gera_resposta(400, {}, "Erro ao criar recomendação.")
-
-@app.route('/recomendacao', methods=['GET'])
-def get_recomendacoes():
-    try:
-        recomendacoes = Recomendacao.query.all()
-        if not recomendacoes:
-            return gera_resposta(404, [], "Nenhuma recomendação encontrada.")
-        
-        recomendacoes_json = [r.to_json() for r in recomendacoes]
-        return gera_resposta(200, recomendacoes_json)
-    except Exception as e:
-        print('Erro:', e)
-        return gera_resposta(500, {}, "Ocorreu um erro interno no servidor.")
-
-@app.route('/recomendacao/<int:id>', methods=['GET'])
-def get_recomendacao_por_id(id):
-    try:
-        recomendacao = Recomendacao.query.get(id)
-        if not recomendacao:
-            return gera_resposta(404,{}, f"Recomendação com ID {id} não encontrada.")
-        return gera_resposta(200, recomendacao.to_json())
-    except Exception as e:
-        print('Erro:', e)
-        return gera_resposta(500, {}, "Ocorreu um erro interno no servidor.")
-
     
-# -----------------------------
-# Função auxiliar de resposta
-# -----------------------------
-def gera_resposta(status, conteudo, mensagem=False):
-    body = {"conteudo": conteudo}
-    if mensagem:
-        body["mensagem"] = mensagem
-    return Response(json.dumps(body), status=status, mimetype="application/json")
+
 
 if __name__ == "__main__":
     app.run(port=5000, host="localhost", debug=True)
